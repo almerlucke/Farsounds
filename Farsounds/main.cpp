b3f079f6b8d937f0c2e0097206533c6c73382d8c
Almer Lucke <almer.lucke@gmail.com>
2013-02-13 20:51:45 +0100

    Initial Commit

---
+++ Farsounds/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Farsounds
+//
+//  Created by aFrogleap on 2/13/13.
+//  Copyright (c) 2013 Farcoding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
