4009b94bf3653a376f544924ab1f5424aae35a0a
Almer Lucke <almer.lucke@gmail.com>
2013-02-22 21:45:13 +0100

    added another spawnfactory example

---
+++ Farsounds/Patches/spawnfactory_example2.h
@@ -0,0 +1,14 @@
+//
+//  spawnfactory_example2.h
+//  Farsounds
+//
+//  Created by aFrogleap on 2/22/13.
+//  Copyright (c) 2013 Farcoding. All rights reserved.
+//
+
+#ifndef __Farsounds__spawnfactory_example2__
+#define __Farsounds__spawnfactory_example2__
+
+void spawnfactory_example2();
+
+#endif /* defined(__Farsounds__spawnfactory_example2__) */
